10cf7e70610478a20ad07a81200335bf99619997
dh1994y <1419570349@qq.com>
2015-12-11 23:46:34 +0800

    整合界面连接数据库获取菜单

--- oa/WebContent/resource/js/oa.js
+++ oa/WebContent/resource/js/oa.js
@@ -18,6 +18,8 @@
 
 
 function createMenu(config){
+	//辅助参数
+	var x = 1;
 	//解析config参数
 	var mainNavList = config.mainNavList;//一维数组 name
 	var mainNavCount = mainNavList.length;//主菜单数目
@@ -67,34 +69,90 @@ function createMenu(config){
 		});
 		
 		
-		//创建leftNavItemDom   左侧需要定位到 iframe 模块
-		var leftNavItemDom = $('<li id="'+lid+'" iframeMark="'+fid+'"></li>');
+		//创建leftNavItemDom   
+		var leftNavItemDom = $('<li id="'+lid+'"></li>');
 		leftNavContainerDom.append(leftNavItemDom);
 		var leftNavItemUlDom = $('<ul class="nav nav-pills nav-stacked"></ul>');
 		leftNavItemDom.append(leftNavItemUlDom);
 		for(var j=0;j<leftNavList[i].length;j++){
-			//创建左侧导航子菜单项
-			var leftNavItemUlLiDom = $('<li url="'+leftNavList[i][j].url+'"><a href="javascript:void(0);">'+leftNavList[i][j].name+'</a></li>');
+			//创建左侧导航子菜单项  需要包含创建右上navid did+rt+x 和 右下id did+rb+x 即需要定位到did
+			var lnavid = lid+"lnav0"+(j+1);
+			var leftNavItemUlLiDom = $('<li id="'+lnavid+'" flag="close" didMark="'+did+'" url="'+leftNavList[i][j].url+'"><a href="javascript:void(0);">'+leftNavList[i][j].name+'</a></li>');
 			leftNavItemUlDom.append(leftNavItemUlLiDom);
 			//创建子菜单项点击事件
 			leftNavItemUlLiDom.click(function(){
+				
+				if($(this).attr("flag")=="open"){
+					//alert(11);
+					//定位
+					//当前为活动其余非活动样式
+					$(this).addClass("active").siblings().removeClass("active");
+					$("#"+$(this).attr("drtidMark")).removeClass("myli-close").addClass("myli-active").siblings().removeClass("myli-active").addClass("myli-close");
+					$("#"+$(this).attr("drbidMark")).removeClass("li-hidden").addClass("li-show").siblings().removeClass("li-show").addClass("li-hidden");
+					return;
+				}
+				$(this).attr("flag","open");
 				//当前项未被选中时生效
 				if(!$(this).hasClass("active")){
 					//当前为活动其余非活动样式
 					$(this).addClass("active").siblings().removeClass("active");
-					//获取iframe跳转url
-					var url = $(this).attr("url");
-					//获取对应iframe id
-					var iframeId = $(this).parent().parent().attr("iframeMark");
-					//设置url
-					$("#"+iframeId).attr("src",url);
+					//创建右上id
+					var drtid = $(this).attr("didMark")+"rt"+x;
+					//创建左上id
+					var drbid = $(this).attr("didMark")+"rb"+x;
+					x++;//更新x保证id唯一
+					if(x==100){
+						x=1;
+					}
+					//创建右上标签
+					//alert($(this).children(":first").text());
+					var rtLabelDom = $('<li id="'+drtid+'" class="myli-active" drbidMark="'+drbid+'" lidMark="'+$(this).attr("id")+'"><a>'+$(this).children(":first").text()+'</a><img src="/oa/resource/image/close.png"/></li>');
+					$(this).attr("drtidMark",drtid).attr("drbidMark",drbid);
+					//添加到容器
+					$("#"+$(this).attr("didMark")).children(":first").children(":first").append(rtLabelDom);
+					rtLabelDom.siblings().removeClass("myli-active").addClass("myli-close");
+					//为右上标签添加点击事件
+					rtLabelDom.children(":first").click(function(){
+						//触发左导航点击事件
+						//alert("添加"+$(this).parent().attr("lidMark"));
+						$("#"+$(this).parent().attr("lidMark")).click();
+					});
+					//为右上标签close图标添加点击事件
+					rtLabelDom.children(":last").click(function(){
+						//alert("关闭");
+						//若为当前选中项 移除时将左侧导航active移除
+						var lnavItemDom = $("#"+$(this).parent().attr("lidMark"));
+						lnavItemDom.attr("flag","close");
+						if(lnavItemDom.hasClass("active")){
+							//alert("移除样式");
+							lnavItemDom.removeClass("active");
+						}
+						//移除iframe 先移除iframe避免 移除导航条后找不到 this
+						$("#"+$(this).parent().attr("drbidMark")).remove();
+						//选中上一个标签
+						//alert($(this).parent().next());
+						if($(this).parent().next().length>0){
+							$(this).parent().next().children(":first").click();
+						}else if($(this).parent().prev().length>0){
+							$(this).parent().prev().children(":first").click();
+						}
+						//移除导航条
+						$($(this).parent()).remove();	
+					});
+					
+					
+					//创建右下iframe
+					var rbIframeDom = $('<li id="'+drbid+'" class="li-show"><iframe src="'+$(this).attr("url")+'"></iframe></li>');
+					//添加到容器
+					$("#"+$(this).attr("didMark")).children(":last").children(":first").append(rbIframeDom);
+					rbIframeDom.siblings().removeClass("li-show").addClass("li-hidden");
+					
 				}
 			});
 		}
 		
-		
 		//创建rightContentDom
-		var rightContentDom = $('<li id="'+rid+'"><div id="'+did+'" class=".content-div"><iframe id="'+fid+'" src="#"></iframe></div></li>');
+		var rightContentDom = $('<li id="'+rid+'"><div id="'+did+'" class=".content-div">'+'<div id="page-middle-right-top"><ul id="page-middle-right-nav" ></ul></div><div id="page-middle-right-below"><ul id="page-middle-right-content"></ul></div>');
 		rightContentContainerDom.append(rightContentDom);
 	
 		//创建完成自动点击第一项
--- oa/src/com/nsn/oa/service/MenuService.java
+++ oa/src/com/nsn/oa/service/MenuService.java
@@ -1,8 +1,15 @@
 package com.nsn.oa.service;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.nsn.oa.dao.IMenuDao;
+import com.nsn.oa.dao.utils.Conditions;
+import com.nsn.oa.dao.utils.Conditions.Operator;
+import com.nsn.oa.domain.Menu;
 import com.nsn.oa.domain.utils.MenuConfig;
+import com.nsn.oa.domain.utils.MenuItem;
 
 public class MenuService {
 	private IMenuDao menuDao;
@@ -11,8 +18,58 @@ public class MenuService {
 	 * 获取菜单配置
 	 */
 	public MenuConfig getMenuConfig() {
-		
-		return null;
+		//创建MenuConfig对象
+		MenuConfig config = new MenuConfig();
+		List<String> mainNavList = new ArrayList<>();
+		List<List<MenuItem>> leftNavList = new ArrayList<>();
+		Conditions conditions = new Conditions();
+		conditions.addCondition("isUse", 1, Operator.EQUALS);
+		conditions.addCondition("level", 1, Operator.EQUALS);
+		conditions.addOrderBy("orderValue", true);
+		List<Menu> menuList = menuDao.findByConditions(conditions);
+		for(Menu menu : menuList){
+			mainNavList.add(menu.getName());
+			List<MenuItem> menuItemList = new ArrayList<>();
+			leftNavList.add(menuItemList);
+			conditions.clear();
+			conditions.addCondition("isUse", 1, Operator.EQUALS);
+			conditions.addCondition("parentId", menu.getId(), Operator.EQUALS);
+			conditions.addOrderBy("orderValue", true);
+			List<Menu> mList = menuDao.findByConditions(conditions);
+			for(Menu m : mList){
+				MenuItem menuItem = new MenuItem();
+				menuItem.setName(m.getName());
+				menuItem.setUrl(m.getUrl());
+				menuItemList.add(menuItem);
+			}
+		}
+		/*
+		//按条件检索出所有菜单
+		Conditions conditions = new Conditions();
+		conditions.addCondition("isUse", 1, Operator.EQUALS);
+		conditions.addOrderBy("level", true);
+		conditions.addOrderBy("orderValue", true);
+		List<Menu> menuList = menuDao.findByConditions(conditions);
+		//设置MenuConfig对象内容
+		for (Menu menu : menuList) {
+			if(menu.getLevel()==1){
+				mainNavList.add(menu.getName());
+				List<MenuItem> menuItemList = new ArrayList<>();
+				leftNavList.add(menuItemList);
+				for(Menu m : menuList){
+					if(m.getParentId().equals(menu.getId())){
+						MenuItem menuItem = new MenuItem();
+						menuItem.setName(m.getName());
+						menuItem.setUrl(m.getUrl());
+						menuItemList.add(menuItem);
+					}
+				}
+			}
+		}
+		*/
+		config.setLeftNavList(leftNavList);
+		config.setMainNavList(mainNavList);
+		return config;
 	}
 
 	public IMenuDao getMenuDao() {
